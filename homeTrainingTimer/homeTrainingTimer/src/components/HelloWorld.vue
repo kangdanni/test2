a74a9cff05013a1d09b16fd56805cd54a802e705
강단 <kkangdanni@sk.com>
2020-04-14 17:26:08 +0900

    timer

---
+++ homeTrainingTimer/homeTrainingTimer/src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="hello">
+    {{ seconds }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: "",
+      timeCounter: 180 //3분
+    };
+  },
+  mounted() {
+    this.myTimer();
+  },
+
+  methods: {
+    myTimer() {
+      this.count++;
+      if (this.count < 181) {
+        this.timeCounter = this.timeCounter - 1;
+        setTimeout(this.myTimer, 1000);
+      } else {
+        console.log("end");
+      }
+    }
+  },
+
+  computed: {
+    seconds() {
+      return this.modifiedDate;
+    },
+
+    modifiedDate: function() {
+      // return Math.trunc(this.now)
+      return (
+        Math.trunc(this.timeCounter / 60) +
+        " : " +
+        ("0" + (this.timeCounter % 60)).slice(-2)
+      );
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
